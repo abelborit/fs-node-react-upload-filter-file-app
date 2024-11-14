462bf1656f63936c36f767cd9cf3f2927503594d
Abel Borit <abelboritguitton@gmail.com>
2024-11-14 18:14:26 -0500

    [update] uploadSingleFile - FileUploadController

--- backend/src/presentation/file-upload/controller.ts
+++ backend/src/presentation/file-upload/controller.ts
@@ -6,8 +6,53 @@ export class FileUploadController {
   constructor() {}
 
   public uploadSingleFile(request: Request, response: Response) {
-    response
-      .status(201)
-      .json({ data: [], message: "file uploated successfully" });
+    const { file } = request;
+    // console.log({ file });
+
+    const validExtentions: string[] = ["csv"];
+
+    try {
+      if (!file) {
+        throw { data: null, message: "file is required" };
+      }
+
+      const fileExtension = file.mimetype.split("/").at(1) ?? ""; // para tomar la segunda posición por ejemplo image/jpeg -> jpeg y si no viene nada entonces que le coloque un string vacío
+
+      if (!validExtentions.includes(fileExtension)) {
+        throw { data: null, message: "it is not a valid extention" };
+      }
+
+      const { fieldname, originalname, mimetype, size, ...restOfArgs } = file;
+
+      response.status(201).json({
+        // data: file,
+        data: { fieldname, originalname, mimetype, size },
+        message: "file uploated successfully",
+      });
+
+      return;
+    } catch (error: any) {
+      console.log("error in controller in file-upload", { error });
+
+      switch (error.message) {
+        case "file is required":
+          response
+            .status(404)
+            .json({ data: null, message: "file is required" });
+          return;
+
+        case "it is not a valid extention":
+          response
+            .status(404)
+            .json({ data: null, message: "it is not a valid extention" });
+          return;
+
+        default:
+          response
+            .status(500)
+            .json({ data: null, message: "Internal Server Error" });
+          return;
+      }
+    }
   }
 }
