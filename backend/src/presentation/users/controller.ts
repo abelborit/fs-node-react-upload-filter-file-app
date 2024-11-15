25d72be0f7fe6c26fc7d3b15bccd2de99a4f9dd9
Abel Borit <abelboritguitton@gmail.com>
2024-11-14 21:56:07 -0500

    [update] getUsers in arrow function

--- backend/src/presentation/users/controller.ts
+++ backend/src/presentation/users/controller.ts
@@ -3,9 +3,10 @@ import { Request, Response } from "express";
 export class UsersController {
   constructor() {}
 
-  public getUsers(request: Request, response: Response) {
+  /* se está pasando como función flecha en lugar de función tradicional para poder tener correctamente el contexto del ".this" sin necesidad de hacer el ".bind(.....)" en las rutas */
+  public getUsers = (request: Request, response: Response) => {
     response
       .status(200)
       .json({ data: [], message: "users obtained successfully" });
-  }
+  };
 }
