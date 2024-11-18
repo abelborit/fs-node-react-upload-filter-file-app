7383e185468f3cef8be7d748bd75a8e93c0689c0
Abel Borit <abelboritguitton@gmail.com>
2024-11-18 15:56:12 -0500

    [add] APP_STATUS constant

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,10 @@
+/* el uso de "as const" asegura que los valores dentro de APP_STATUS sean tratados como valores literales y no como simples string, lo que también nos debería permitir que el autocompletado funcione correctamente al querer usar el "APP_STATUS" en otros archivo */
+export const APP_STATUS = {
+  INIT: "init", // al entrar a la aplicación
+  ERROR: "error", // si hay un error
+  READY_TO_UPLOAD: "ready_to_upload", // al elegir el archivo y antes de subirlo
+  UPLOADING: "uploading", // mientras se está subiendo el archivo
+  READY_TO_USAGE: "ready_to_usage", // después de subir el archivo
+} as const;
+
+export type AppStatusType = (typeof APP_STATUS)[keyof typeof APP_STATUS];
