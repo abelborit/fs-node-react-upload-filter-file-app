c0f2ff123d9d28f56cb0b0214822e4713c5dadfa
Abel Borit <abelboritguitton@gmail.com>
2024-11-18 17:39:45 -0500

    [update] fileUploadService

--- frontend/src/services/file-upload.service.ts
+++ frontend/src/services/file-upload.service.ts
@@ -1,9 +1,9 @@
-type DataFileResponse = Array<Record<string, string>>;
+import { FileDataResponseInterface } from "../interfaces/FileDataResponse.interface";
 
-/* lo que regresaremos será un tupla para tener el error y la data en caso haya uno o el otro */
+/* lo que regresaremos será un tupla para tener el error y la data en caso haya uno o el otro. Tener en cuenta que TypeScript no permite que un elemento obligatorio venga después de un elemento opcional en una tupla. En una tupla, los elementos opcionales deben ir al final */
 export const fileUploadService = async (
   file: File
-): Promise<[Error?, DataFileResponse?]> => {
+): Promise<[FileDataResponseInterface, Error?]> => {
   const formData = new FormData();
   formData.append("file", file);
 
@@ -20,7 +20,7 @@ export const fileUploadService = async (
     const data = await response.json();
     console.log("Response from server", data);
 
-    return [undefined, data];
+    return [data, undefined];
   } catch (error: unknown) {
     /* En TypeScript, error dentro del bloque catch tiene el tipo unknown por defecto. Esto significa que TypeScript no sabe qué tipo de objeto es el error */
     console.error("Error file-upload.service.ts - frontend", error);
@@ -28,11 +28,14 @@ export const fileUploadService = async (
     /* Verificación de tipo para asegurarnos de que el error es una instancia de Error */
     /* Este es un type guard que verifica si el error es una instancia de la clase Error. Si es así, lo tratamos como un error y lo devolvemos de la forma correcta */
     if (error instanceof Error) {
-      return [error, []]; // Si es un error, lo devolvemos correctamente
+      return [{} as FileDataResponseInterface, error]; // Si es un error, lo devolvemos correctamente
     }
 
     /* Si no es un Error, devolvemos un Error genérico */
     /* Si el error no es una instancia de Error (lo cual podría suceder en algunos casos, como errores lanzados explícitamente con otros tipos), devolvemos un error genérico con un mensaje adecuado */
-    return [new Error("An unknown error occurred"), []];
+    return [
+      {} as FileDataResponseInterface,
+      new Error("An unknown error occurred"),
+    ];
   }
 };
